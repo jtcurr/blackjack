3073b30d712508db82ea9008e270f2446484b60b
John Currie <johntcurrie@gmail.com>
2017-03-22 21:00:31 +0900

    Added comments for server

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const port = 3030;
 let app = express();
 
 app.use(express.static(__dirname));
-
+//create a redirect to index
 app.get('/', function(req, res){
 	res.redirect('index.html');
 })
-
+//connect server
 app.listen(port, function(){
 	console.log('Listening on port ', port);
 })
